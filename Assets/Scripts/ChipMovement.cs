64ed107e69ec62f16ab48c2514909577c4cf7f87
Andrew Colannino <akcolannino@email.neit.edu>
2017-07-12 15:12:20 -0400

    3d chip movement, 2d chip sprite (based on 3d)

---
+++ Assets/Scripts/ChipMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChipMovement : MonoBehaviour
+{
+	const float distFromCamera = 20;
+	Vector3 lastTargetPos;
+	Vector2 mousePos;
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+			Input.mousePosition.y, Camera.main.nearClipPlane + distFromCamera));
+
+		Vector3 deltaMovement = targetPos - lastTargetPos;
+		deltaMovement *= 20;
+		transform.rotation = Quaternion.Euler(deltaMovement.y, -deltaMovement.x, 0);
+
+		transform.position = targetPos;
+		lastTargetPos = targetPos;
+	}
+}
